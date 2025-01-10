63329e45ff61f3b577addb24ceafecbc4260f1c0
Sazzad Hossain <developersazzad7@gmail.com>
2025-01-10 16:40:30 +0600

    post Navigate buttons and GoBack buttons adds

--- src/components/Post/Post.jsx
+++ src/components/Post/Post.jsx
@@ -1,13 +1,17 @@
 import PropTypes from "prop-types";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 const Post = ({ post }) => {
     const { id, title, userId } = post;
+    const navigate = useNavigate();
     const postStyle = {
         border: '1px solid #ccc',
         borderRadius : '10px',
         margin: '10px',
         padding: '10px'
+    }
+    const handleCustomButton = () => {
+        navigate(`/post/${id}`)
     }
     return (
         <div style={postStyle}>
@@ -16,6 +20,8 @@ const Post = ({ post }) => {
             <Link to={`/post/${id}`}>
                 <button>Show Details</button>
             </Link>
+            <br /><br />
+            <button onClick={handleCustomButton}>View Posts Details</button>
         </div>
     );
 };
--- src/components/PostDetails/PostDetails.jsx
+++ src/components/PostDetails/PostDetails.jsx
@@ -1,13 +1,20 @@
-import { useLoaderData } from "react-router-dom";
+import { useLoaderData, useNavigate } from "react-router-dom";
 
 const PostDetails = () => {
     const post = useLoaderData();
+    const navigate = useNavigate();
+
     const {id, title, body} = post;
+
+    const handleGoBack = () => {
+        navigate(-1);
+    }
     return (
         <div>
             <h4>Post Id is : {id}</h4>
             <h5>{title}</h5>
             <p>{body}</p>
+            <button onClick={handleGoBack}>Go Back</button>
         </div>
     );
 };
